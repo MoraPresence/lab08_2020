8c8cc66e3a8594ac9abc3adc2d206caa661369a9
MoraPresence <od34156la@mail.ru>
2020-03-08 20:44:46 +0300

    version 0.0.1

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,71 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <iostream>
+#include <boost/asio.hpp>
+#include <utility>
+#include <boost/thread/pthread/recursive_mutex.hpp>
+#include <algorithm>
+#include <boost/log/support/date_time.hpp>
+#include <thread>
+#include <boost/thread/thread.hpp>
 
+namespace ip = boost::asio::ip;
+using Context = boost::asio::io_context;
+using Socket = boost::asio::ip::tcp::socket;
+using Resolver = boost::asio::ip::tcp::resolver;
+using StreamBuffer = boost::asio::streambuf;
+using Buffer = boost::asio::mutable_buffer;
+
+class client {
+public:
+    client(std::shared_ptr<Context> &io_context, Socket socket)
+    : _socket(std::move(socket)),
+    _starrted(true), _resolver(*io_context) {}
+
+
+    void connect(const ip::tcp::endpoint &);
+
+    void loop();
+
+    void close();
+
+    Socket &getSocket();
+
+    void answerFromServer();
+
+    [[nodiscard]]  bool isClose();
+
+    void login();
+
+    void ping();
+
+    void getClients();
+
+    std::string getName();
+
+    void setName(const std::string &username) {
+        _username = username;
+    }
+
+    void setTime(time_t timeNow) {
+        _lastTime = timeNow;
+    }
+
+    void setStart(bool health) {
+        _starrted = health;
+    }
+
+    [[nodiscard]] bool timed_out() const;
+
+    void stop();
+
+private:
+    std::string _username = "user";
+    Socket _socket;
+    bool _starrted;
+    std::shared_ptr<Context> _io_context = std::make_shared<Context>();;
+    Resolver _resolver;
+    time_t _lastTime;
+};
 #endif // INCLUDE_HEADER_HPP_
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,107 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <header.hpp>
+#define KICK_FROM_SERVER  -100
+
+void client::connect(const ip::tcp::endpoint &ep) {
+    _socket.connect(ep);
+}
+
+void client::loop() {
+
+}
+
+void client::close() {
+    _socket.close();
+}
+
+Socket &client::getSocket() {
+    return _socket;
+}
+
+void client::answerFromServer() {
+    boost::asio::streambuf buffer{};
+    std::ostream out(&buffer);
+    while (true) {
+        try {
+            time_t seconds = time(NULL);
+            std::cout << time(&seconds);
+            std::cout << "Enter msg: ";
+            std::string word;
+            std::cin >> word;
+            word += "\r\n\r\n";
+            out << word;
+            write(this->getSocket(), buffer);
+            if (word.find("login", 0) != -1)
+                login();
+            else if (word.find("clients", 0) != -1)
+                getClients();
+            else std::cout << "invalid msg: " << word << std::endl;
+            buffer.consume(buffer.size());
+            boost::this_thread::sleep(boost::posix_time::millisec(rand()%7000));
+            ping();
+        } catch (std::runtime_error &exception) {
+            this->stop();
+            exit(KICK_FROM_SERVER);
+        }
+    }
+}
+
+
+void client::login() {
+    boost::asio::streambuf buffer{};
+    std::ostream out(&buffer);
+    std::string name;
+    std::cin >> name;
+    name += "\r\n\r\n";
+    out << name;
+    this->setName(name);
+    write(this->getSocket(), buffer);
+    buffer.consume(buffer.size());
+    read_until(this->getSocket(), buffer, "\n");
+    std::string message{std::istreambuf_iterator<char>{&buffer},
+                        std::istreambuf_iterator<char>{}};
+    std::cout << message << std::endl;
+    buffer.consume(buffer.size());
+}
+
+void client::ping() {
+    boost::asio::streambuf buffer{};
+    std::ostream out(&buffer);
+    out << "Ping Ok!\n";
+    write(this->getSocket(), buffer);
+    buffer.consume(buffer.size());
+    read_until(this->getSocket(), buffer, "\n");
+    std::string message{std::istreambuf_iterator<char>{&buffer},
+                        std::istreambuf_iterator<char>{}};
+    std::cout << message << std::endl;
+    buffer.consume(buffer.size());
+}
+
+void client::getClients() {
+    boost::asio::streambuf buffer{};
+    read_until(this->getSocket(), buffer, "\n");
+    std::string message{std::istreambuf_iterator<char>{&buffer},
+                        std::istreambuf_iterator<char>{}};
+    std::cout << message << std::endl;
+    buffer.consume(buffer.size());
+}
+
+[[nodiscard]] bool client::isClose() {
+    return !this->getSocket().is_open();
+}
+
+void client::stop() {
+    boost::system::error_code err;
+    _socket.close(err);
+}
+
+bool client::timed_out() const {
+    time_t now = time(NULL);
+    long long ms = now - _lastTime;
+    return ms > 5000;
+}
+
+std::string client::getName() {
+    return _username;
+}
